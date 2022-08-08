5f5fac9d0ab93c61aa187bf58ac8cb339b2dd916
Ruslan Mukhametshin <ruslanow597@gmail.com>
2022-08-08 22:04:16 +0300

    Didn't do hw-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // task 1
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+
+            // task 2 Подсмотрел решение преподавателя. Не понятен принцип. Незачёт.
+
+
+            // task 3
+            // Вообще не понял условия, с чего начинать
+        }
     }
 }
