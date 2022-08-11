9939e9ff8f2f314c358c7b62eee9197b71637159
Ruslan Mukhametshin <ruslanow597@gmail.com>
2022-08-11 20:54:28 +0300

    Hw-4 Done.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         // task 1, куда мыслить дальше, застрял. Циклы дались тяжело!!!
@@ -17,11 +19,33 @@ public class Main {
             System.out.print(i + " ");
         }
 
-        // task 2 Подсмотрел решение преподавателя. Не понятен принцип. Слишком заумно. Я это не проходил. Незачёт.
+        System.out.println();
 
+        // task 2 Подсмотрел решение преподавателя. Не понятен принцип. Незачёт)
+        int firstFridayDate = 5;
+
+        for (int currentFriday = firstFridayDate; currentFriday <= 31; currentFriday += 7) {
+            System.out.println("Сегодня пятница, " + currentFriday + "-е число. Необходимо подготовить отчет.");
+        }
 
         // task 3
-        // Вообще не понял условия, с чего начинать. Математика страдает(((
+        // Вообще не понял условия, с чего начинать. Про методы LocalDate & get Year не слышал.
+        // Как мне решать похожим образом?
+        int currentYear = LocalDate.now().getYear();
+        int lastAppearanceYear = 0;
+        for (int year = 0; year <= currentYear + 100; year += 79) {
+            lastAppearanceYear = year;
+        }
+        int previousAppearanceYear = 0;
+        for (int year = 0; year < currentYear - 200; year += 79) {
+            previousAppearanceYear = year;
+        }
+
+        // Вывести ниже все появления комет за предыдущие 200 лет + след. появление кометы
+        for (int year = previousAppearanceYear; year <= lastAppearanceYear; year += 79) {
+            System.out.println(year);
+        }
+
 
     }
 }
